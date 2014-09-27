213b68c5747ae9d1ab81d2e9ba9430d6cb2de9bf
tdjahel <tristan.djahel@epitech.eu>
2014-09-27 10:41:28 +0200

    Changements techniques : - Intégration d'un "pull to refresh" sur la liste des contenus - Changement de méthode de stockage du cache (Utilisation de la classe storagefolder) - Développement de deux fonctions dans la classe tools (GetObjectFromByteArray<T> et GetBytesFromObject<T>)
    Changements sur la GUI :
    - Implémentation de la police et des couleurs donné par Bruno
    - Implémentation d'une barre de chargement pour indiquer pour le contenu est en train d'être récupéré sur le Web
    - Mis en place du panorama sur un contenu
    - Suppression de la page "MainPage.xaml" (Pas utile)
    	-> Avant, le clear de cache était sur cette page, maintenant cette opération est disponible à l'écran de liste des contenus
    	-> Changement du fichier WMAppManifest.xml pour changer la Navigation Page

---
+++ Pumgrana/Pumgrana/Pumgrana/Article.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows.Documents;
+
+namespace Pumgrana
+{
+    public partial class Article : PhoneApplicationPage
+    {
+        DT_Article DT = new DT_Article();
+        PumgranaWebClient wc = new PumgranaWebClient();
+        string id = "";
+        static private int IdArticle = 0;
+        static private Dictionary<string, int> WebIdToLocalId = new Dictionary<string, int>();
+        public Article()
+        {
+            InitializeComponent();
+            wc.Error += wc_Error;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            NavigationContext.QueryString.TryGetValue("id", out id);
+            
+            if (WebIdToLocalId.ContainsKey(id) == true)
+            {
+                this.DT = await (IsolatedStorageOperations.Load<DT_Article>("Article_" + WebIdToLocalId.First(tmp => tmp.Key == id).Value.ToString() + ".xml"));
+            }
+            this.ProgressLoadContent.Visibility = System.Windows.Visibility.Visible;
+
+            LoadArticle();
+
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            wc.Error -= wc_Error;
+            base.OnNavigatedFrom(e);
+        }
+
+        private void LoadArticle()
+        {
+            GetDetailsFromId();
+            GetTagsFromId();
+            GetLinkedArticlesFromId();
+        }
+
+        void wc_Error(object sender, object output)
+        {
+            Exception e = output as Exception;
+            MessageBox.Show(e.Message);
+            this.ProgressLoadContent.Visibility = System.Windows.Visibility.Collapsed;
+            (this.ApplicationBar.Buttons[0] as ApplicationBarIconButton).IsEnabled = true;
+        }
+
+        private void GetDetailsFromId()
+        {
+            wc.GetDetail += wc_GetDetail;
+            wc.getDetail(id);
+        }
+
+        void wc_GetDetail(object sender, object output)
+        {
+            wc.GetDetail -= wc_GetDetail;
+            Content res = (output as ListContent).contents[0];
+            this.ContentPanoramaItem.DataContext = res;
+            PhoneApplicationService.Current.State["Article_Content"] = ContentPanoramaItem.DataContext;
+            this.DT.content = res;
+            this.TitleOfArticle.DataContext = this.DT.content;
+
+            string html = "<html><head><meta charset=\"utf-8\"/></head><body>" + res.body + "</body></html>";
+            this.ArticleWebView.NavigateToString(html);
+        }
+
+        private void GetTagsFromId()
+        {
+            wc.GetTagsFromContent += wc_GetTagsFromContent;
+            wc.getTagsFromContent(id);
+        }
+
+        void wc_GetTagsFromContent(object sender, object output)
+        {
+            wc.GetTagsFromContent -= wc_GetTagsFromContent;
+            ListTag lt = output as ListTag;
+            if (lt.tags != null)
+                DT.listTag.ToList().ForEach(lt.tags.Add);
+            this.TagsPanoramaItem.DataContext = lt;
+            PhoneApplicationService.Current.State["Article_Tag"] = TagsPanoramaItem.DataContext;
+        }
+
+        private void GetLinkedArticlesFromId()
+        {
+            wc.GetLinksFromContent += wc_GetLinksFromContent;
+            wc.getLinksFromContent(id);
+        }
+
+        void wc_GetLinksFromContent(object sender, object output)
+        {
+            wc.GetLinksFromContent -= wc_GetLinksFromContent;
+            ListLink ll = output as ListLink;
+            this.LinkedPanoramaItem.DataContext = ll;
+            PhoneApplicationService.Current.State["Article_Link"] = LinkedPanoramaItem.DataContext;
+            this.DT.listLink = ll;
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(SaveCurrentPage), this.DT);
+        }
+
+        private void SaveCurrentPage(object state)
+        {
+            IsolatedStorageOperations.Save<DT_Article>(this.DT, "Article_" + IdArticle.ToString() + ".xml");
+            if (WebIdToLocalId.ContainsKey(id) == false)
+            {
+                WebIdToLocalId.Add(id, IdArticle++);
+            }
+        }
+
+        private void ApplicationBarIconButton_Click_1(object sender, System.EventArgs e)
+        {
+            (sender as ApplicationBarIconButton).IsEnabled = false;
+            this.ProgressLoadContent.Visibility = System.Windows.Visibility.Visible;
+            LoadArticle();
+        }
+
+        private void ArticleWebView_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            (this.ApplicationBar.Buttons[0] as ApplicationBarIconButton).IsEnabled = true;
+            this.ProgressLoadContent.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void LongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LongListSelector list = sender as LongListSelector;
+            Link l = list.SelectedItem as Link;
+
+            wc.GetLinkDetailFromUri += wc_GetLinkDetailFromUri;
+            wc.getLinkDetailFromUri(l.link_uri);
+        }
+
+        void wc_GetLinkDetailFromUri(object sender, object output)
+        {
+            wc.GetLinkDetailFromUri -= wc_GetLinkDetailFromUri;
+            FullLinkList data = output as FullLinkList;
+            NavigationService.Navigate(new Uri("/Article.xaml?id=" + data.links[0].target_uri, UriKind.Relative));
+        }
+    }
+}
---
+++ Pumgrana/Pumgrana/Pumgrana/PageManager.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Xml.Serialization;
+using Windows.Storage;
+
+namespace Pumgrana
+{
+    public static class IsolatedStorageOperations
+    {
+        public static async void Save<T>(this T obj, string filename)
+        {
+            // Get the data to serialize
+            byte[] data = Tools.GetBytesFromObject<T>(obj);
+
+            // Get the local folder
+            StorageFolder local = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            // Create a new file named DataFile.txt.
+            var file = await local.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+
+            // Write the data from the textbox.
+            using (var s = await file.OpenStreamForWriteAsync())
+            {
+                s.Write(data, 0, data.Length);
+                s.Close();
+            }            
+        }
+
+        public static async Task<T> Load<T>(string filename)
+        {
+            // Get the local folder.
+            StorageFolder local = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            if (local != null)
+            {
+                string data;
+
+                // Get the file.
+                try
+                {
+                    var file = await local.OpenStreamForReadAsync(filename);
+
+                    System.Diagnostics.Debug.WriteLine("Load " + filename + " from local");
+
+                    // Read the data.
+                    StreamReader sr = new StreamReader(file);
+                    data = sr.ReadToEnd();
+                    sr.Close();
+                    var obj = (T)(Tools.GetObjectFromByteArray<T>(Tools.GetBytes(data)));
+                    return obj;
+                }
+                catch (System.IO.FileNotFoundException e)
+                {
+                    System.Diagnostics.Debug.WriteLine("Load " + filename + " from web");
+                    T voidobject = Activator.CreateInstance<T>();
+                    return voidobject;
+                }
+            }
+            T voidobject2 = Activator.CreateInstance<T>();
+            return voidobject2;
+        }
+    }
+}
